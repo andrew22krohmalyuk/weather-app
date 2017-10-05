cc5421de76f84b36abed441fbe4f83c4ff76557c
Andrew Krohmalyuk <andrew.krohmalyuk12@gmail.com>
2017-10-05 20:04:57 +0400

    Refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,11 @@ import { render } from "react-dom";
 
 import SearchBar from '../containers/SearchBar';
 import WeatherList from '../containers/WeatherList';
-import CurrentWeather from '../containers/CurrentWeather';
 
 export default class App extends Component  {
     render() {
         return (
             <div className="app">
-                <CurrentWeather />
                 <div className="wrapper">
                     <SearchBar />
                     <WeatherList />
